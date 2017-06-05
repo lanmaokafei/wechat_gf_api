e22b332c59f83271d47d7ca3e5c90d834505dd9b
lanmaokafei <roheike@qq.com>
2017-06-05 22:31:15 +0800

    修正api_demo中引用问题 新增secret用于保存微信的appid和secret

---
+++ weixin_secret.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@version: ??
+@author: xyj
+@license: MIT License
+@contact: xieyingjun@vip.qq.com
+@Created on 2017/6/5
+"""
+
+appid = ''  # 微信公众号的appid
+secret = ''  # 公众号后台自行设定的，在后台页面找到
